c84188cd119e796f36d0aaab6beb3da74f4887d8
serafer <serafer3@gmail.com>
2023-05-08 17:01:10 -0300

    test 5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SuperiorNavbar from './components/SuperiorNavbar/SuperiorNavbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import './App.css';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import CartContainer from './components/CartContainer/CartContainer';
 import Checkout from './components/Checkout/Checkout';
@@ -17,13 +17,13 @@ function App() {
         <CartProvider>
           <SuperiorNavbar />
           <Navbar />
-          <Router basename='/index.html'>
+          <Routes basename='//serafer/alefer-app-test'>
             <Route path="/" element={<ItemListContainer greeting={'Andes Drink Co | E-Commerce'} />} />
             <Route path="/category/:category" element={<ItemListContainer greeting={'Productos filtrados por categoria'} />} />
             <Route path="/item/:itemId" element={<ItemDetailContainer />} />
             <Route path="/cart" element={<CartContainer  greeting={'Cart'}/> } />
             <Route path="/Checkout" element={<Checkout  greeting={'Checkout'}/> } />
-          </Router>
+          </Routes>
         </CartProvider>
       </BrowserRouter>
     </div>
